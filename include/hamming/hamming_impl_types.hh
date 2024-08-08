1b684e46859f78a9c9d5a5836683d2f0324c2bf3
Trevor Kramer <tkramer@relaytx.com>
2024-08-08 19:54:47 -0400

    Amino acids

--- include/hamming/hamming_impl_types.hh
+++ include/hamming/hamming_impl_types.hh
@@ -8,7 +8,7 @@ namespace hamming {
 // 4-bit representation of gene:
 using GeneBlock = std::uint8_t;
 using SparseData = std::vector<std::size_t>;
-constexpr std::size_t n_bits_per_gene{4};
+constexpr std::size_t n_bits_per_gene{20};
 constexpr GeneBlock mask_gene0{0x0f};
 constexpr GeneBlock mask_gene1{0xf0};
 
